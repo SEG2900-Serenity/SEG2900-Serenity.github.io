38e70cd39529d71489ea68d2e1c3f3b847db63e5
Michael H <114048139+MichaelHaddad47@users.noreply.github.com>
2022-10-30 22:49:17 -0400

    Update home.js

--- html/home.js
+++ html/home.js
@@ -2,6 +2,8 @@ const Title = document.getElementById('Title');
 
 Title.addEventListener('mouseover', function handleMouseOver() {
   Title.style.color = 'red';
+});
   
 Title.addEventListener('mouseout', function handleMouseOut() {
   Title.style.color = 'black';
+});
