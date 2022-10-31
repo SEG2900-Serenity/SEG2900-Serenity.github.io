0e9df4184c8c17da72d42199f9b54ce0fe05f241
Michael H <114048139+MichaelHaddad47@users.noreply.github.com>
2022-10-30 22:48:44 -0400

    Create home.js

---
+++ html/home.js
@@ -0,0 +1,7 @@
+const Title = document.getElementById('Title');
+
+Title.addEventListener('mouseover', function handleMouseOver() {
+  Title.style.color = 'red';
+  
+Title.addEventListener('mouseout', function handleMouseOut() {
+  Title.style.color = 'black';
